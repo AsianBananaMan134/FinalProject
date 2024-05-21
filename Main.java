a9046af1e249d0fe675927b1584a7684500f5b11
Allen U <91634751+AsianBananaMan134@users.noreply.github.com>
2024-05-21 14:57:51 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * This is the main method of the overall project.
+ * This project would create a seating chart with the
+ * students that are attending the class(aka present).
+ *
+ * @author Allen Ukigai
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the teacher name?: ");
+        String teacherName = input.nextLine();
+
+        System.out.println("How wide do you want the classroom to be?: ");
+        int width = input.nextInt();
+
+        System.out.println("How long do you want the classroom to be?: ");
+        int length = input.nextInt();
+
+        Classroom classroom = new Classroom(teacherName, width, length);
+        classroom.assignSeats();
+        classroom.setAttendance();
+        classroom.updateStudents();
+        classroom.getAttendance();
+        classroom.introductions();
+        classroom.getStudentList();
+
     }
 }
